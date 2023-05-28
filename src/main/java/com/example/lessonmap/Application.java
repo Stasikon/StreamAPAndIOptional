a6e8772ae36afcdeca91cda482af793241afd2df
Heppy <lecsa515@gmail.com>
2023-05-28 23:24:47 +0300

    Start Commit

---
+++ src/main/java/com/example/lessonmap/Application.java
@@ -0,0 +1,13 @@
+package com.example.lessonmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
